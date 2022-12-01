155486207b720249f1a0a522b62ae8f0170e665c
Denperidge <denperidge@gmail.com>
2022-12-01 15:51:26 +0100

    p32

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
 
-export default class BandsRoute extends Route {}
+export default class BandsRoute extends Route {
+    model() {
+        return [
+            { name: 'Newgrounds Death Rugby' },
+            { name: 'food house' },
+            { name: 'CORNER STORE KINGDOM' }
+        ];
+    }
+}
