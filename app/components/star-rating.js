da48d222dd5634a734607bc154bfe124b46aaddc
Denperidge <denperidge@gmail.com>
2022-12-02 16:14:31 +0100

    Linting

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -1,19 +1,19 @@
 import Component from '@glimmer/component';
 
 export default class StarRatingComponent extends Component {
-    get maxRating() {
-        return this.args.maxRating ?? 5;
-    }
+  get maxRating() {
+    return this.args.maxRating ?? 5;
+  }
 
-    get stars() {
-        let stars = [];
-        for (let i = 1; i <= this.maxRating; i++) {
-            stars.push({
-                rating: i,
-                full : i <= this.args.rating,
-            });
-        }
-        console.log(stars)
-        return stars;
+  get stars() {
+    let stars = [];
+    for (let i = 1; i <= this.maxRating; i++) {
+      stars.push({
+        rating: i,
+        full: i <= this.args.rating,
+      });
     }
+    console.log(stars);
+    return stars;
+  }
 }
