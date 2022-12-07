b9f135cf573aa3836370ad5a0f3e7c18577f92eb
Denperidge <denperidge@gmail.com>
2022-12-07 17:08:25 +0100

    p171

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -4,7 +4,6 @@ import { inject as service } from '@ember/service';
 export default class BandsBandRoute extends Route {
   @service catalog;
 
-
   model(params) {
     // This route was generated with a dynamic segment. Implement data loading
     // based on that dynamic segment here in the model hook.
