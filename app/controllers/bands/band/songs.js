dc4ec69f0ebc79ad8f8ee7eb6889536b80e03de0
Denperidge <denperidge@gmail.com>
2022-12-07 12:14:42 +0100

    Not implemented: p151 are-we-there-yet-6

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -30,7 +30,13 @@ export default class BandsBandSongsController extends Controller {
       { title: this.title },
       { band: { data: { id: this.model.id, type: 'bands' } } }
     );
-    
+    /*
+      Not implemented: p151 are-we-there-yet-6 
+      (because I'm not sure what is meant with this but that might be a me problem)
+      "Similarly, when we save a song to the back-end where the `band`
+      relationship is set, the songs relationship of the band could
+      be updated automatically to onclude the song"
+    */
     this.model.songs = [...this.model.songs, song];
     this.title = '';
     this.showAddSong = true;
