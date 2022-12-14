3a89c0c01a558899f3735e945493bef4cfb7497c
Denperidge <denperidge@gmail.com>
2022-12-14 16:52:01 +0100

    p209, with custom code

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -4,6 +4,35 @@ import { setupApplicationTest } from 'rarwe/tests/helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { getPageTitle } from 'ember-page-title/test-support';
 
+async function dataTestSteps(...args) {
+  for (let i = 0; i < args.length; i+=2) {
+    let func = args[i];
+    let target = args[i+1];
+    let selector = `[data-test-rr="${target}"]`;
+
+    // If no additional parameter
+    if (typeof args[i+2] === 'function') { 
+      await func(selector);
+    }
+    // If additional parameter
+    else {
+      await func(selector, args[i+2]);
+      i++;  // Skip the additional parameter next step
+    }
+
+  }
+  /*
+  await args.forEach(async (step) => {
+    let func, target, value;
+    [ func, target, value ] = step;
+
+    target = `[data-test-rr="${args[0]}"]`;
+    func = args[1];
+    await func(target);
+  });
+  */
+}
+
 module('Acceptance | bands', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
@@ -32,10 +61,12 @@ module('Acceptance | bands', function (hooks) {
     this.server.create('band', { name: 'Royal Blood' });
 
     await visit('/');
-    await click('a[href="/bands/new"]');
-    await fillIn('input', 'Caspian');
-    await click('button');
-    await waitFor('p.text-center');
+    await dataTestSteps(
+      click, "new-band-button",
+      fillIn, "new-band-name", 'Caspian',
+      click, "save-band-button",
+      waitFor, "no-songs-text"
+    );
 
     let bandLinks = document.querySelectorAll('.mb-2 > a');
     assert.strictEqual(
@@ -49,7 +80,7 @@ module('Acceptance | bands', function (hooks) {
       'The new band link is rendered as the last item'
     );
     assert.ok(
-      document.querySelector('.border-b-4.border-purple-400')
+      document.querySelector('[data-test-rr="songs-nav-item"]')
         .textContent.includes('Songs'),
       'The Songs tab is active'
     )
