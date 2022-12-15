09b636ea49f7803fea011bf54ce111251da7840c
Denperidge <denperidge@gmail.com>
2022-12-15 13:35:26 +0100

    p230, renamed custom song-item to song-list-item

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -1,4 +1,4 @@
-import { module, test } from 'qunit';
+import { assert, module, test } from 'qunit';
 import { visit, waitFor, click, fillIn } from '@ember/test-helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupApplicationTest } from 'rarwe/tests/helpers';
@@ -24,9 +24,9 @@ module('Acceptance | songs', function (hooks) {
       click,
       'songs-nav-item'
     );
-    await waitFor(testSelector('song-item'));
+    await waitFor(testSelector('song-list-item'));
 
-    assert.dom(testSelector('song-item')).hasText('DESKTOP!!!');
+    assert.dom(testSelector('song-list-item')).hasText('DESKTOP!!!');
   });
 
   test('Create a song', async function (assert) {
@@ -42,8 +42,42 @@ module('Acceptance | songs', function (hooks) {
 
     await createSong('DESKTOP!!!');
 
-    await waitFor(testSelector('song-item'));
+    await waitFor(testSelector('song-list-item'));
 
-    assert.dom(testSelector('song-item')).hasText('DESKTOP!!!');
+    assert.dom(testSelector('song-list-item')).hasText('DESKTOP!!!');
+  });
+
+  test('Sort songs in various ways', async function () {
+    let band = this.server.create('band', { name: 'Them Crooked Vultures' });
+
+    [
+      ['Mind Eraser, No Chaser', 2],
+      ['Elephants', 4],
+      ['Spinning in Daffodils', 5],
+      ['New Fang', 3]
+    ].forEach((songInfo) => {
+      this.server.create('song', {
+        title: songInfo[0],
+        rating: songInfo[1],
+        band
+      });
+    });
+
+    await visit('/');
+
+    await click(testSelector('band-link'));
+    
+    assert
+      .dom(testSelector('song-list-item', ':first-child'))
+      .hasText(
+        'Elephants',
+        'The first song is the one that comes first in the alphabet'
+      );
+    assert
+      .dom(testSelector('song-list-item', ':last-child'))
+      .hasText(
+        'Spinning in Daffodils',
+        'The last song is the one that comes last in the alphabet'
+      );
   });
 });
