1de1e4f46a625e2a2ba459c082d1c514dc07174a
Denperidge <denperidge@gmail.com>
2022-12-15 17:43:25 +0100

    Removed sort attribute from SortButton

--- app/components/sort-button.js
+++ app/components/sort-button.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 
 export default class SortButtonComponent extends Component {
     get icon() {
-        if (!this.args.sort.startsWith('-')) {
+        if (!this.args.testSelector.endsWith('desc')) {
             return 'angle-up';
         } else {
             return 'angle-down';
